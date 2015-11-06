957d20f6f581a44f4acdf942e000550938f43216
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-11-06 08:26:26 +0100

    Add node-purge-ttl setting support

--- puppetdb.sh
+++ puppetdb.sh
@@ -5,4 +5,8 @@ if test -n "${ENABLE_HTTP}" && ${ENABLE_HTTP}; then
   sed -i -E 's/^#?\s*host = .*/host = 0.0.0.0/' /etc/puppetlabs/puppetdb/conf.d/jetty.ini
 fi
 
+if test -n "${NODE_PURGE_TTL}"; then
+  echo "node-purge-ttl = ${NODE_PURGE_TTL}" >> /etc/puppetlabs/puppetdb/conf.d/database.ini
+fi
+
 /opt/puppetlabs/server/bin/puppetdb foreground

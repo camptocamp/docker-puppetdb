756e62b9a8db455d1e1e08405a443636f31a4c52
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-01-22 15:32:59 +0100

    Use exec

--- puppetdb.sh
+++ puppetdb.sh
@@ -9,4 +9,4 @@ if test -n "${NODE_PURGE_TTL}"; then
   echo "node-purge-ttl = ${NODE_PURGE_TTL}" >> /etc/puppetlabs/puppetdb/conf.d/database.ini
 fi
 
-/opt/puppetlabs/server/bin/puppetdb foreground
+exec /opt/puppetlabs/server/bin/puppetdb foreground

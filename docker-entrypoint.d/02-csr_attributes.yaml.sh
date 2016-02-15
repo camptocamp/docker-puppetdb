5f225f762a64d5be51065c0c78a0a074e3caa9e0
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-15 13:29:09 +0100

    Fix ssl certificate

--- docker-entrypoint.d/02-csr_attributes.yaml.sh
+++ docker-entrypoint.d/02-csr_attributes.yaml.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 if getent hosts rancher-metadata; then
+  # Generate csr_attributes.yaml
   echo $(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null) > /etc/puppetlabs/puppet/csr_attributes.yaml
+
+  # Get certificate
+  if getent hosts puppetca; then
+    puppet agent -t --noop --server puppetca
+  else
+    puppet agent -t --noop
+  fi
 fi

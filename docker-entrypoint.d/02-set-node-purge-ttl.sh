79d753b7d80d248013b6cd584e793597b1642f8b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-10-26 16:19:55 +0200

    Fix 02-set-node-purge-ttl.sh

--- docker-entrypoint.d/02-set-node-purge-ttl.sh
+++ docker-entrypoint.d/02-set-node-purge-ttl.sh
@@ -2,5 +2,5 @@
 
 if test -n "${NODE_PURGE_TTL}"; then
   augtool -Ast "Puppet.lns incl /etc/puppetlabs/puppetdb/conf.d/database.ini" \
-    set "/files//database.ini/database/node-purge-ttl"
+    set "/files//database.ini/database/node-purge-ttl" "${NODE_PURGE_TTL}"
 fi

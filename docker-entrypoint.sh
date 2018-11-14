86e1f89a25e081e5e9eee6086c55b4ba43fa9468
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2018-11-14 11:54:53 +0100

    Support running as arbitrary user
    Also upgrade to puppetdb 5.2.6, switch to ubuntu bionic for the base image.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-DIR=/docker-entrypoint.d
+#DIR=/docker-entrypoint.d
+#
+#if [[ -d "$DIR" ]]
+#then
+#  /bin/run-parts --regex '\.(sh|rb)$' "$DIR"
+#fi
 
-if [[ -d "$DIR" ]]
-then
-  /bin/run-parts --regex '\.(sh|rb)$' "$DIR"
-fi
+set -a
 
-exec "$@"
+source /etc/default/puppetdb
+source "${INSTALL_DIR}/ezbake-functions.sh"
+
+
+CLASSPATH="${INSTALL_DIR}/puppetdb.jar"
+
+COMMAND="${JAVA_BIN} ${JAVA_ARGS} ${LOG_APPENDER} \
+         -Djava.security.egd=/dev/urandom \
+         -cp "$CLASSPATH" \
+         clojure.main -m puppetlabs.puppetdb.main \
+         --config ${CONFIG} --bootstrap-config ${BOOTSTRAP_CONFIG} \
+         ${TK_ARGS}"
+
+exec ${COMMAND}

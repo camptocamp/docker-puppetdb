cef0bb79d17fc167efcb4cc7a4310b5d31bdec9e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-07-01 12:00:22 +0200

    Adapt for puppetdb 6.10

--- docker-entrypoint.d/02-set-node-purge-ttl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if test -n "${NODE_PURGE_TTL}"; then
-  augtool -Ast "Puppet.lns incl /etc/puppetlabs/puppetdb/conf.d/database.ini" \
-    set "/files//database.ini/database/node-purge-ttl" "${NODE_PURGE_TTL}"
-fi
--- docker-entrypoint.d/09-csr_attributes.yaml.sh
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-if getent hosts rancher-metadata > /dev/null; then
-  # Generate csr_attributes.yaml
-  cat << EOF > /etc/puppetlabs/puppet/csr_attributes.yaml
----
-custom_attributes:
-  1.2.840.113549.1.9.7: '$(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null)'
-EOF
-
-  # Get certificate
-  rc=1
-  while test $rc -ne 0; do
-    if getent hosts puppetca > /dev/null; then
-      puppet agent -t --noop --server puppetca
-    else
-      puppet agent -t --noop
-    fi
-    rc=$?
-  done
-elif test -n "${AUTOSIGN_PSK}"; then
+if test -n "${AUTOSIGN_PSK}"; then
   # Generate csr_attributes.yaml
   cat << EOF > /etc/puppetlabs/puppet/csr_attributes.yaml
 ---
@@ -30,13 +12,8 @@ extension_requests:
 EOF
 
   # Get certificate
-  rc=1
-  while test $rc -ne 0; do
-    if getent hosts puppetca > /dev/null; then
-      puppet agent -t --noop --server puppetca
-    else
-      puppet agent -t --noop
-    fi
-    rc=$?
-  done
+	if test -n "${DNS_ALT_NAMES}"; then
+		DNS_ALT_NAMES_FLAG="--dns_alt_names=${DNS_ALT_NAMES}"
+	fi
+  puppet ssl submit_request --environment production $DNS_ALT_NAMES_FLAG --waitforcert 60
 fi
